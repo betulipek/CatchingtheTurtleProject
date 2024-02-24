d9098908b0d30aefb75cdf13c5eccad97d16591b
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-24 15:29:24 +0300

    Screen Window height automatically fixed and added Penup to delete the Turtle arrow

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ score_turtle = turtle.Turtle()
 
 score_turtle.hideturtle()
 score_turtle.color("dark blue")
+score_turtle.penup()
+
+top_height = screen.window_height() / 2
+y = top_height * 0.80
+
+score_turtle.setpos(0,y)
 score_turtle.write(arg="SCORE: 0", move=False, align="center", font=FONT)
 
 
