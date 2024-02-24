cf3a5198121c68769a96db43d42de49e663517f5
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-24 15:39:55 +0300

    created setup_score_turtle

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ FONT = ("Verdana", 30, "bold")
 
 #Score Turtle
 score_turtle = turtle.Turtle()
+def setup_score_turtle():
 
-score_turtle.hideturtle()
-score_turtle.color("dark blue")
-score_turtle.penup()
+    score_turtle.hideturtle()
+    score_turtle.color("dark blue")
+    score_turtle.penup()
 
-top_height = screen.window_height() / 2
-y = top_height * 0.80
+    top_height = screen.window_height() / 2
+    y = top_height * 0.80
 
-score_turtle.setpos(0,y)
-score_turtle.write(arg="SCORE: 0", move=False, align="center", font=FONT)
 
+    score_turtle.setpos(0,y)
+    score_turtle.write(arg="SCORE: 0", move=False, align="center", font=FONT)
+
+setup_score_turtle()
 
 
 turtle.mainloop()
