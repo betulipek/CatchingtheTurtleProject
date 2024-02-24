cacc24cdbfbb230c35dfc09191f892cda92939ea
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-24 15:13:43 +0300

    How to hide a turtle

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ FONT = ("Verdana", 30, "bold")
 
 #Score Turtle
 score_turtle = turtle.Turtle()
+
+score_turtle.hideturtle()
 score_turtle.color("dark blue")
 score_turtle.write(arg="SCORE: 0", move=False, align="center", font=FONT)
 
