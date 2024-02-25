8303044a153d29d68b2c84adde258e8bc46bccc5
Betul Ipek Cakar <ayhancakar73@gmail.com>
2024-02-25 13:25:09 +0300

    Making new turtle in a new structure started

--- main.py
+++ main.py
@@ -3,24 +3,37 @@ import turtle
 screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catching the Turtle")
-FONT = ("Verdana", 30, "bold")
+FONT = ("Verdana", 20, "bold")
+grid_size = 10
 
 #Score Turtle
 score_turtle = turtle.Turtle()
 def setup_score_turtle():
-
     score_turtle.hideturtle()
     score_turtle.color("dark blue")
     score_turtle.penup()
 
     top_height = screen.window_height() / 2
     y = top_height * 0.80
-
-
     score_turtle.setpos(0,y)
     score_turtle.write(arg="SCORE: 0", move=False, align="center", font=FONT)
 
-setup_score_turtle()
+def make_turtle(x,y):
+    new_turtle_in_new_structure = turtle.Turtle()
+    new_turtle_in_new_structure.penup()
+    new_turtle_in_new_structure.shape("turtle")
+    new_turtle_in_new_structure.shapesize(2,2)
+    new_turtle_in_new_structure.color("green")
+    new_turtle_in_new_structure.goto(x * grid_size, y * grid_size)
 
+
+
+
+setup_score_turtle()
+make_turtle(-20,20)
+make_turtle(-10,20)
+make_turtle(0,20)
+make_turtle(10,20)
+make_turtle(20,20)
 
 turtle.mainloop()
